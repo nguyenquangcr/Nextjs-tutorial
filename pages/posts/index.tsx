a2c1d43e64271ed9aa78c7bea848dc310953c800
QuangTN-FPT <quangtn12@fpt.com.vn>
2023-03-08 23:39:12 +0700

    Static Site Generation(SSG) get StaticProps

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,10 +1,13 @@
+import { GetStaticProps, GetStaticPropsContext } from 'next';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
 
-export interface PostsProps {}
+export interface PostsProps {
+  posts: any[];
+}
 
-export default function PostsPage(props: PostsProps) {
+export default function PostsPage({ posts }: PostsProps) {
   const router = useRouter();
 
   function goToDetailPage() {
@@ -19,7 +22,12 @@ export default function PostsPage(props: PostsProps) {
 
   return (
     <>
-      <div>Posts Page</div>
+      <div>Posts List Page</div>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
       <button
         onClick={() => {
           goToDetailPage();
@@ -30,3 +38,21 @@ export default function PostsPage(props: PostsProps) {
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps<PostsProps> = async (
+  context: GetStaticPropsContext
+) => {
+  //server-side
+  //run when build time
+  const response = await fetch('https://js-post-api.herokuapp.com/api/posts?_page=1');
+  const data = await response.json();
+
+  return {
+    props: {
+      posts: data?.data.map((x: any) => ({
+        id: x.id,
+        title: x.title,
+      })),
+    },
+  };
+};
