9840f0509cc0401e34a0dad9265113c5fac13d58
QTN-dev <quangtn12@fpt.com.vn>
2023-04-03 18:09:35 +0700

    Update UI medicine

--- components/common/HighProducts/index.tsx
+++ components/common/HighProducts/index.tsx
@@ -3,7 +3,7 @@ import { Typography, Card, Col, Row } from 'antd';
 import { HighlightFilled, BulbFilled } from '@ant-design/icons';
 import './index.css';
 
-export interface HeaderProps {}
+export interface HeaderProps { }
 
 const { Meta } = Card;
 
@@ -22,6 +22,7 @@ const headerImage: React.CSSProperties = {
 const styleCol: React.CSSProperties = {
   marginTop: '16px',
 };
+
 
 export default function HighProductComponent(props: HeaderProps) {
   return (
@@ -68,6 +69,7 @@ export default function HighProductComponent(props: HeaderProps) {
           {/* ROW ONE */}
           <Col style={styleCol} lg={6} xs={12}>
             <Card
+              className='custom-card'
               hoverable
               style={{ width: 192, boxShadow: '0 0 0 1px #d8e0e8' }}
               cover={
@@ -78,14 +80,26 @@ export default function HighProductComponent(props: HeaderProps) {
                 />
               }
             >
-              <Meta
-                title="Siro bổ phế Bối Mẫu Forte Mom And Baby Tất Thành hỗ trợ giảm ho, đau rát họng, khản tiếng (125ml)"
-                description="55.000d / Chai"
-              />
+              <div className='class-note'>
+                1 Chai x 1 Chai
+              </div>
+              <Typography.Title
+                level={4}
+                className='nameMidicine'
+              >
+                Siro bổ phế Bối Mẫu Forte Mom And Baby Tất Thành hỗ trợ giảm ho, đau rát họng, khản tiếng (125ml)
+              </Typography.Title>
+              <Typography.Title
+                level={4}
+                className='class-price'
+              >
+                <span>55.000</span> /Chai
+              </Typography.Title>
             </Card>
           </Col>
           <Col style={styleCol} lg={6} xs={12}>
             <Card
+              className='custom-card'
               hoverable
               style={{ width: 192, boxShadow: '0 0 0 1px #d8e0e8' }}
               cover={
@@ -96,60 +110,86 @@ export default function HighProductComponent(props: HeaderProps) {
                 />
               }
             >
-              <Meta
-                title="Siro Healthy New Kids hỗ trợ kích thích tiêu hóa, giúp ăn ngon (120ml)"
-                description="81.000d / Chai"
-              />
+              <div className='class-note'>
+                Chai
+              </div>
+              <Typography.Title
+                level={4}
+                className='nameMidicine'
+              >Siro Healthy New Kids hỗ trợ kích thích tiêu hóa, giúp ăn ngon (120ml)
+              </Typography.Title>
+              <Typography.Title
+                level={4}
+                className='class-price'
+              >
+                <span>81.000</span> /Chai
+              </Typography.Title>
             </Card>
           </Col>
           <Col style={styleCol} lg={6} xs={12}>
             <Card
+              className='custom-card'
               hoverable
               style={{ width: 192, boxShadow: '0 0 0 1px #d8e0e8' }}
               cover={
                 <img
                   style={{ height: '192px', padding: '10px' }}
                   alt="example"
-                  src="https://cdn.nhathuoclongchau.com.vn/unsafe/fit-in/600x600/filters:quality(90):fill(white)/nhathuoclongchau.com.vn/images/product/2021/04/00030511-lacto-biomin-gold-new-hdpharma-20-goi-2174-607c_large.jpg"
+                  src="https://cdn.nhathuoclongchau.com.vn/unsafe/fit-in/600x600/filters:quality(90):fill(white)/nhathuoclongchau.com.vn/images/product/2022/05/00345910-xit-hong-xuyen-tam-lien-hai-thuong-vuong-30ml-5572-6272_large.jpg"
                 />
               }
             >
-              <Meta
-                title="Dung dịch xịt Xuyên Tâm Liên Hải Thượng Vương Royal Care giảm ho, giảm đờm (30ml)"
-                description="97.000d / Hộp"
-              />
+              <div className='class-note'>
+                Chai
+              </div>
+              <Typography.Title
+                level={4}
+                className='nameMidicine'
+              >
+                Dung dịch xịt Xuyên Tâm Liên Hải Thượng Vương Royal Care giảm ho, giảm đờm (30ml)
+              </Typography.Title>
+              <Typography.Title
+                level={4}
+                className='class-price'
+              >
+                <span>97.000</span> /Chai
+              </Typography.Title>
             </Card>
           </Col>
           <Col style={styleCol} lg={6} xs={12}>
             <Card
+              className='custom-card'
               hoverable
               style={{ width: 192, boxShadow: '0 0 0 1px #d8e0e8' }}
               cover={
                 <img
                   style={{ height: '192px', padding: '10px' }}
                   alt="example"
-                  src="https://cdn.nhathuoclongchau.com.vn/unsafe/fit-in/600x600/filters:quality(90):fill(white)/nhathuoclongchau.com.vn/images/product/2022/10/00021988-anica-phytextra-60v-5137-6347_large.jpg"
+                  src="https://cdn.nhathuoclongchau.com.vn/unsafe/fit-in/600x600/filters:quality(90):fill(white)/nhathuoclongchau.com.vn/images/product/2022/10/00031036-omexxel-ginkgo-120-2x15-5958-633e_large.jpg"
                 />
               }
             >
-              <Meta
-                title="Viên uống Omexxel Ginkgo 120 Excelife hỗ trợ tăng cường tuần hoàn máu não, tốt cho tim mạch (30 viên)"
-                description="364.000d / Hộp"
-              />
+              <div className='class-note'>
+                Chai
+              </div>
+              <Typography.Title
+                level={4}
+                className='nameMidicine'
+              >
+                Viên uống Omexxel Ginkgo 120 Excelife hỗ trợ tăng cường tuần hoàn máu não, tốt cho tim mạch (30 viên)
+              </Typography.Title>
+              <Typography.Title
+                level={4}
+                className='class-price'
+              >
+                <span>291.200</span> / Hộp
+              </Typography.Title>
             </Card>
           </Col>
-          {/* <Col style={styleCol} lg={4}>
-                    <Card
-                        hoverable
-                        style={{ width: 192, boxShadow: '0 0 0 1px #d8e0e8' }}
-                        cover={<img style={{ height: '192px', padding: '10px' }} alt="example" src="https://cdn.nhathuoclongchau.com.vn/unsafe/fit-in/600x600/filters:quality(90):fill(white)/nhathuoclongchau.com.vn/images/product/2022/12/00028684-vien-uong-omega-3-6-9-naturecare-giam-cholesterol-bao-ve-tim-mach-60-vien-9210-63a9_large.jpg" />}
-                    >
-                        <Meta title="Viên sủi Optimax Immunity Booster Vid - Fighter tăng cường sức đề kháng (20 viên)" description="118.000d / Tuýp" />
-                    </Card>
-                </Col> */}
           {/* ROW TWO */}
           <Col style={styleCol} lg={6} xs={12}>
             <Card
+              className='custom-card'
               hoverable
               style={{ width: 192, boxShadow: '0 0 0 1px #d8e0e8' }}
               cover={
@@ -160,14 +200,26 @@ export default function HighProductComponent(props: HeaderProps) {
                 />
               }
             >
-              <Meta
-                title="Siro bổ phế Bối Mẫu Forte Mom And Baby Tất Thành hỗ trợ giảm ho, đau rát họng, khản tiếng (125ml)"
-                description="55.000d / Chai"
-              />
+              <div className='class-note'>
+                1 Chai x 1 Chai
+              </div>
+              <Typography.Title
+                level={4}
+                className='nameMidicine'
+              >
+                Siro bổ phế Bối Mẫu Forte Mom And Baby Tất Thành hỗ trợ giảm ho, đau rát họng, khản tiếng (125ml)
+              </Typography.Title>
+              <Typography.Title
+                level={4}
+                className='class-price'
+              >
+                <span>55.000</span> /Chai
+              </Typography.Title>
             </Card>
           </Col>
           <Col style={styleCol} lg={6} xs={12}>
             <Card
+              className='custom-card'
               hoverable
               style={{ width: 192, boxShadow: '0 0 0 1px #d8e0e8' }}
               cover={
@@ -178,57 +230,82 @@ export default function HighProductComponent(props: HeaderProps) {
                 />
               }
             >
-              <Meta
-                title="Siro Healthy New Kids hỗ trợ kích thích tiêu hóa, giúp ăn ngon (120ml)"
-                description="81.000d / Chai"
-              />
+              <div className='class-note'>
+                Chai
+              </div>
+              <Typography.Title
+                level={4}
+                className='nameMidicine'
+              >Siro Healthy New Kids hỗ trợ kích thích tiêu hóa, giúp ăn ngon (120ml)
+              </Typography.Title>
+              <Typography.Title
+                level={4}
+                className='class-price'
+              >
+                <span>81.000</span> /Chai
+              </Typography.Title>
             </Card>
           </Col>
           <Col style={styleCol} lg={6} xs={12}>
             <Card
+              className='custom-card'
               hoverable
               style={{ width: 192, boxShadow: '0 0 0 1px #d8e0e8' }}
               cover={
                 <img
                   style={{ height: '192px', padding: '10px' }}
                   alt="example"
-                  src="https://cdn.nhathuoclongchau.com.vn/unsafe/fit-in/600x600/filters:quality(90):fill(white)/nhathuoclongchau.com.vn/images/product/2021/04/00030511-lacto-biomin-gold-new-hdpharma-20-goi-2174-607c_large.jpg"
+                  src="https://cdn.nhathuoclongchau.com.vn/unsafe/fit-in/600x600/filters:quality(90):fill(white)/nhathuoclongchau.com.vn/images/product/2022/05/00345910-xit-hong-xuyen-tam-lien-hai-thuong-vuong-30ml-5572-6272_large.jpg"
                 />
               }
             >
-              <Meta
-                title="Dung dịch xịt Xuyên Tâm Liên Hải Thượng Vương Royal Care giảm ho, giảm đờm (30ml)"
-                description="97.000d / Hộp"
-              />
+              <div className='class-note'>
+                Chai
+              </div>
+              <Typography.Title
+                level={4}
+                className='nameMidicine'
+              >
+                Dung dịch xịt Xuyên Tâm Liên Hải Thượng Vương Royal Care giảm ho, giảm đờm (30ml)
+              </Typography.Title>
+              <Typography.Title
+                level={4}
+                className='class-price'
+              >
+                <span>97.000</span> /Chai
+              </Typography.Title>
             </Card>
           </Col>
           <Col style={styleCol} lg={6} xs={12}>
             <Card
+              className='custom-card'
               hoverable
               style={{ width: 192, boxShadow: '0 0 0 1px #d8e0e8' }}
               cover={
                 <img
                   style={{ height: '192px', padding: '10px' }}
                   alt="example"
-                  src="https://cdn.nhathuoclongchau.com.vn/unsafe/fit-in/600x600/filters:quality(90):fill(white)/nhathuoclongchau.com.vn/images/product/2022/10/00021988-anica-phytextra-60v-5137-6347_large.jpg"
+                  src="https://cdn.nhathuoclongchau.com.vn/unsafe/fit-in/600x600/filters:quality(90):fill(white)/nhathuoclongchau.com.vn/images/product/2022/10/00031036-omexxel-ginkgo-120-2x15-5958-633e_large.jpg"
                 />
               }
             >
-              <Meta
-                title="Viên uống Omexxel Ginkgo 120 Excelife hỗ trợ tăng cường tuần hoàn máu não, tốt cho tim mạch (30 viên)"
-                description="364.000d / Hộp"
-              />
+              <div className='class-note'>
+                Chai
+              </div>
+              <Typography.Title
+                level={4}
+                className='nameMidicine'
+              >
+                Viên uống Omexxel Ginkgo 120 Excelife hỗ trợ tăng cường tuần hoàn máu não, tốt cho tim mạch (30 viên)
+              </Typography.Title>
+              <Typography.Title
+                level={4}
+                className='class-price'
+              >
+                <span>291.200</span> / Hộp
+              </Typography.Title>
             </Card>
           </Col>
-          {/* <Col style={styleCol} lg={4}>
-                    <Card
-                        hoverable
-                        style={{ width: 192, boxShadow: '0 0 0 1px #d8e0e8' }}
-                        cover={<img style={{ height: '192px', padding: '10px' }} alt="example" src="https://cdn.nhathuoclongchau.com.vn/unsafe/fit-in/600x600/filters:quality(90):fill(white)/nhathuoclongchau.com.vn/images/product/2022/12/00028684-vien-uong-omega-3-6-9-naturecare-giam-cholesterol-bao-ve-tim-mach-60-vien-9210-63a9_large.jpg" />}
-                    >
-                        <Meta title="Viên sủi Optimax Immunity Booster Vid - Fighter tăng cường sức đề kháng (20 viên)" description="118.000d / Tuýp" />
-                    </Card>
-                </Col> */}
         </Row>
       </div>
     </div>
