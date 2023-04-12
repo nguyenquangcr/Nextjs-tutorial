c611e32ac8d7b637ab1b0a3c4e50345daa3150ef
QTN-dev <quangtn12@fpt.com.vn>
2023-04-12 10:33:31 +0700

    Update GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import { store } from '../store';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 
+import Script from 'next/script'
+
 //css
 import '../styles/globals.css';
 import 'antd/dist/reset.css';
@@ -10,10 +12,24 @@ import 'react-toastify/dist/ReactToastify.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-      <ToastContainer />
-    </Provider>
+    <>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-M4PEFENN4B"></Script>
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-M4PEFENN4B');`,
+        }}
+      />
+      <Provider store={store}>
+        <Component {...pageProps} />
+        <ToastContainer />
+      </Provider>
+    </>
+
   );
 }
 
