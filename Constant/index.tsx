c24e28d37b21e825d0db92db8229ced6306d9d88
QTN-dev <15110108@student.hcmute.edu.vn>
2023-09-29 11:40:57 +0700

    Update code

--- Constant/index.tsx
+++ Constant/index.tsx
@@ -1,2 +1,3 @@
-export const domain = 'https://heroku-backend-nestjs.herokuapp.com';
+// export const domain = 'https://heroku-backend-nestjs.herokuapp.com';
+export const domain = 'https://backend-medicine-de6a28283d26.herokuapp.com/';
 export const domainFE = 'https://dathangthuocsi.online';
